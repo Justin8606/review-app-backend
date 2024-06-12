6570279dd0718cc505a563ec8b7020f170851c48
Justin8606 <justinshajuk@gmail.com>
2024-06-12 21:59:55 +0530

    schema turned into model

--- models/review.js
+++ models/review.js
@@ -4,9 +4,12 @@ const schema = mongoose.Schema(
     {
         "name":{type:String,required:true},
         "year":{type:String,required:true},
-        "duration":{type:String,required:true},
+        "duration":{type:String,required:true},             //schema created
         "rating":{type:String,required:true}
         
     }
 )
+
+let reviewModel = mongoose.model("reviews",schema)          //schema turned into model
+module.exports = {reviewModel}
 
