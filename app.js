37cd5999e721172d3d679b6f423730b6e2a5ba6b
Justin8606 <justinshajuk@gmail.com>
2024-06-12 21:55:27 +0530

    schema created

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const cors = require("cors")
 const app = express()           //express function.
 app.use(cors())             //cors is for security,cors function.
 
-app.get("/add",(req,res)=>{
-    res.send("add page")
+app.post("/add",(req,res)=>{
+    res.send("add page")            // /add is a request and response to this is the add page.(get is changed to post(because of security and inputs(doubt)))
 })
 
-app.listen(8080,(req,res)=>{
+app.listen(8080,()=>{
     console.log("Server Started")
 })
---
+++ models/review.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const schema = mongoose.Schema(
+    {
+        "name":{type:String,required:true},
+        "year":{type:String,required:true},
+        "duration":{type:String,required:true},
+        "rating":{type:String,required:true}
+        
+    }
+)
+
