ed7fbda1671e0c15bca03df6b92f52e2d932672f
Justin8606 <justinshajuk@gmail.com>
2024-06-12 23:25:03 +0530

    view completed

--- app.js
+++ app.js
@@ -22,11 +22,22 @@ app.post("/add",(req,res)=>{
     let review = new reviewModel(input) //next we are goint to console review
     // console.log(input)          //check if it is working on or not.there is one more step.app.use(express.json()) 
 
-    review.save()
+    review.save()       //add completed,next is view
 
     console.log(review)     //this step is to console object into the terminal.(actually here actually the model is creating.)(next connect mongoose)
     // res.send("add page")            // /add is a request and response to this is the add page.(get is changed to post(because of security and inputs(doubt)))
     res.json({"status":"success"})
+})
+
+app.post("/view",(req,res)=>{
+    // res.send("view page")       //check in post man,after that res.send is not needed.
+    reviewModel.find().then((data)=>{
+        res.json(data)
+    }).catch(
+        (error)=>{
+            res.json(error)         //data displayed on post man.
+        }
+    )
 })
 
 app.listen(8080,()=>{
