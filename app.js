a0fa8dff80a88083d1e32340cf7b6243c2f27a99
Justin8606 <justinshajuk@gmail.com>
2024-06-12 22:41:44 +0530

    add completed,save is important

--- app.js
+++ app.js
@@ -15,12 +15,15 @@ app.use(cors())             //cors is for security,cors function.
 app.use(express.json())
 
 
-mongoose.connect("mongodb+srv://justin:<password>@cluster0.3jf2qw3.mongodb.net/reviewdb?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://justin:nitsuj21@cluster0.3jf2qw3.mongodb.net/reviewdb?retryWrites=true&w=majority&appName=Cluster0") //next is save
 
 app.post("/add",(req,res)=>{
     let input = req.body
     let review = new reviewModel(input) //next we are goint to console review
     // console.log(input)          //check if it is working on or not.there is one more step.app.use(express.json()) 
+
+    review.save()
+
     console.log(review)     //this step is to console object into the terminal.(actually here actually the model is creating.)(next connect mongoose)
     res.send("add page")            // /add is a request and response to this is the add page.(get is changed to post(because of security and inputs(doubt)))
 })
