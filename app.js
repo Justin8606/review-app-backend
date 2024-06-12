6e20524da763b6db6e1a87da111ca98ec0b7e12e
Justin8606 <justinshajuk@gmail.com>
2024-06-12 21:27:51 +0530

    app.js started

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+
+const app = express()           //express function.
+app.use(cors())             //cors is for security,cors function.
+
+app.get("/add",(req,res)=>{
+    res.send("add page")
+})
+
+app.listen(8080,(req,res)=>{
+    console.log("Server Started")
+})
